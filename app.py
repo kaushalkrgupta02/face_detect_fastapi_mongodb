88a66b1ddd8d6f38847c69810f75e99620745720
Kaushal Gupta <kaushalkrgupta02@gmail.com>
2025-12-04 23:25:41 +0530

    chek2

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from face_check import app
 
 load_dotenv()
 
-# Export app for Vercel - this is required!
-# Vercel will call this app object directly
-__all__ = ['app']
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="127.0.0.1", port=int(os.getenv("PORT", 8080)))
+
